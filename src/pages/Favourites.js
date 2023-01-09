331a97303156aa531a10d840910705bfc6251bc7
x-farzan <farzanhassan245@gmail.com>
2023-01-10 04:06:44 +0500

    add and remove favourites.

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavouritesContext from "../store/favourites-context";
+
 function FavouritesPage() {
+  const favContext = useContext(FavouritesContext);
+
+  let content;
+  if (favContext.totalFavourites === 0) {
+    content = <p>No favorites added yet!</p>;
+  } else {
+    content = <MeetupList meetups={favContext.favourites} />;
+  }
+
   return (
     <div>
       <h1>Favourites Page</h1>
+      {content}
     </div>
   );
 }
